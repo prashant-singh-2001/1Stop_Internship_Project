8da795147aa90f93de4ee3412c917b065859e689
prashant-singh-2001 <ps47600@gmail.com>
2024-04-20 21:43:51 +0530

    Finished!

--- crud/src/main/java/com/employee/crud/controller/EmployeeController.java
+++ crud/src/main/java/com/employee/crud/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin(origins = "http://localhost")
+@CrossOrigin(origins = "*")
 @RestController
 public class EmployeeController {
 
